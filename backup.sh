957e33d0d151a98622b763eb4850af2aca2c6535
6old9art <dhtyud@gmail.com>
2025-07-31 20:55:16 +0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,8 +2,7 @@
 
 cd ~
 sudo apt update
-devtools
-y
+
 sudo apt install MySQL-client-core-8.0
 docker exec mysql1 sh -c 'exec mysqldump --all-databases -uroot -pmy-secret-pw' | gzip > my-joomla.backup.sql.gz
 docker exec joomla1 tar -czf - /var/www/html > joomla-files.tar.gz
